fbb6fe17a5699f2cf2278a3b1ae73a0d3b3b7eef
Benedict Neo 梁耀恩 <benthecoder07@gmail.com>
2023-08-03 10:00:19 -0700

    Add: sqlite3 import

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from chromadb_connection import ChromaDBConnection
 import pandas as pd
